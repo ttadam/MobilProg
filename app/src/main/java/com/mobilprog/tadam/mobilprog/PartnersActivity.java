c98bb6edcaeda51bc69ac24818e11ab6cc83e088
tmartin <tmartin@dell.com>
2017-04-22 19:08:54 +0200

    AsynkTask próbálkozás ksoap2 külső soap jar-al PartnersList letöltéssel

--- app/src/main/java/com/mobilprog/tadam/mobilprog/PartnersActivity.java
+++ app/src/main/java/com/mobilprog/tadam/mobilprog/PartnersActivity.java
@@ -7,17 +7,15 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class PartnersActivity extends AppCompatActivity {
 
@@ -31,6 +29,15 @@ public class PartnersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_partners);
+
+        Button downloadPartners = (Button) findViewById(R.id.partners_download);
+        downloadPartners.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AsyncCallWS task = new AsyncCallWS();
+                task.execute();
+            }
+        });
 
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         optionsTitles = getResources().getStringArray(R.array.options_array);
