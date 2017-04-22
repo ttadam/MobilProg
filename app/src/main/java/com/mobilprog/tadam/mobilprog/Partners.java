2037aefee2ec1aabd3964d95d812b9afc7714b9f
tmartin <tmartin@dell.com>
2017-04-22 14:07:56 +0200

    Partners Parcelable hozzáadva, Style ActionBar nélküli LoginActivity, Registration és ActionBar-os többi Activity

--- app/src/main/java/com/mobilprog/tadam/mobilprog/Partners.java
+++ app/src/main/java/com/mobilprog/tadam/mobilprog/Partners.java
@@ -1,10 +1,13 @@
 package com.mobilprog.tadam.mobilprog;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by thoma on 2017. 04. 19..
  */
 
-public class Partners {
+public class Partners implements Parcelable{
 
     private String picture;
     private String name;
@@ -14,6 +17,23 @@ public class Partners {
         this.name = name;
     }
 
+    protected Partners(Parcel in) {
+        picture = in.readString();
+        name = in.readString();
+    }
+
+    public static final Creator<Partners> CREATOR = new Creator<Partners>() {
+        @Override
+        public Partners createFromParcel(Parcel in) {
+            return new Partners(in);
+        }
+
+        @Override
+        public Partners[] newArray(int size) {
+            return new Partners[size];
+        }
+    };
+
     public String getPicture() {
         return picture;
     }
@@ -22,4 +42,15 @@ public class Partners {
         return name;
     }
 
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(picture);
+        parcel.writeString(name);
+    }
 }
